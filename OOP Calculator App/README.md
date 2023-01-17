c0c757dbdd9937fbcf6d5eb6976c4cfbcf2a08ed
Ahmed Lamlih <contact.ahmed.lamlih@gmail.com>
2023-01-17 02:45:53 +0100

    Commit the OOP Calculator App Files

---
+++ OOP Calculator App/README.md
@@ -0,0 +1,28 @@
+# OOP Calculator App Using Dart
+Yesterday we attended our second session in Flutter 101 Bootcamp organized by GDG Guelmim, in this session we learned about the OOP programming paradigm which we discovered classes (properties, methods, constructors) and how to instantiate an object, then we learned about inheritance where we covered the extends, super, and this keywords, and we covered how to override a method, finally we learned about asynchronous where we learned about async/await keywords. It was a session rich of informations I realy enjoyed it ! At the end of this session we had a task to do which is refactor the calculator from the previous task which perform basic mathematical operations such as addition, subtraction, multiplication, and division, then implement the OOP approche while developing it.
+And here are some screenshots of the app that I created:
+## Dashboard
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/dashboard.PNG?raw=true">
+
+## Overall
+<img src="https://user-images.githubusercontent.com/87146845/212546034-9a452357-4560-4dfb-98e4-2c8da9b2c3a3.png">
+
+## Operations
+
+### Addition Operation
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/add.PNG?raw=true">
+
+### Subtraction Operation
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/sub.PNG?raw=true">
+
+### Multiplication Operation
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/mult.PNG?raw=true">
+
+### Division Operation
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/div.PNG?raw=true">
+
+## Error Handling
+
+### Invalid inputs
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/errorHandling1.PNG?raw=true">
+<img src="https://github.com/Lamlih-Dev/Flutter101_Bootcamp/blob/main/Calculator%20App/images/errorHandling2.PNG?raw=true">
---
+++ OOP Calculator App/calculator.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+
+class Operation {
+  //Properties
+  double? number1;
+  double? number2;
+
+  //Constructor
+  Operation(double number1, double number2) {
+    this.number1 = number1;
+    this.number2 = number2;
+  }
+
+  //Calcule Methode
+  calcule() {}
+}
+
+class Addition extends Operation {
+  Addition(double number1, double number2) : super(number1, number2) {}
+
+  @override
+  calcule() {
+    return (super.number1! + super.number2!);
+  }
+}
+
+class Subtraction extends Operation {
+  Subtraction(double number1, double number2) : super(number1, number2) {}
+
+  @override
+  calcule() {
+    return (super.number1! - super.number2!);
+  }
+}
+
+class Multiplication extends Operation {
+  Multiplication(double number1, double number2) : super(number1, number2) {}
+
+  @override
+  calcule() {
+    return (super.number1! * super.number2!);
+  }
+}
+
+class Division extends Operation {
+  Division(double number1, double number2) : super(number1, number2) {}
+
+  @override
+  calcule() {
+    return (super.number1! / super.number2!);
+  }
+}
+
+List getNumbers() {
+  List numbers = [];
+  printWarning("Please enter the first number: ");
+  numbers.add(double.parse(stdin.readLineSync()!));
+  printWarning("Please enter the second number: ");
+  numbers.add(double.parse(stdin.readLineSync()!));
+
+  return numbers;
+}
+
+//Helpers
+void printSuccess(String text) {
+  print('\x1B[32m$text\x1B[0m');
+}
+
+void printError(String text) {
+  print('\x1B[31m$text\x1B[0m');
+}
+
+void printWarning(String text) {
+  print('\x1B[33m$text\x1B[0m');
+}
+
+void main() {
+  String result;
+  double outputResult = 0;
+  printSuccess(
+      "\n************************************************\n* Hello to the Ahmed LAMLIH's calculator app ! *\n************************************************\n");
+  print(
+      " -> 1. Addition\n -> 2. Subtraction\n -> 3. Multiplication\n -> 4. Division");
+  do {
+    printWarning(
+        "Please choose the equivalent number of your operation (type 'exit' to quite the app): ");
+    result = (stdin.readLineSync()!).toLowerCase();
+    switch (result) {
+      case "1":
+        try {
+          List numbers = getNumbers();
+          Addition addition = Addition(numbers[0], numbers[1]);
+          printSuccess("Addition result is : ${addition.calcule()}");
+        } catch (e) {
+          printError("Invalid input ! Please try again with a number.");
+        }
+        break;
+      case "2":
+        try {
+          List numbers = getNumbers();
+          Subtraction subtraction = Subtraction(numbers[0], numbers[1]);
+          printSuccess("Subtraction result is : ${subtraction.calcule()}");
+        } catch (e) {
+          printError("Invalid input ! Please try again with a number.");
+        }
+        break;
+      case "3":
+        try {
+          List numbers = getNumbers();
+          Multiplication multiplication =
+              Multiplication(numbers[0], numbers[1]);
+          printSuccess(
+              "Multiplication result is : ${multiplication.calcule()}");
+        } catch (e) {
+          printError("Invalid input ! Please try again with a number.");
+        }
+        break;
+      case "4":
+        try {
+          List numbers = getNumbers();
+          if (numbers[1] == 0) {
+            printError("The second number should be > than 0");
+          } else {
+            Division division = Division(numbers[0], numbers[1]);
+            printSuccess("Division result is : ${division.calcule()}");
+          }
+        } catch (e) {
+          printError("Invalid input ! Please try again with a number.");
+        }
+        break;
+      default:
+        if (result != "exit") {
+          printError("Please enter a valid number between 1 and 4.");
+        } else {
+          printSuccess("We will miss you ! Thank you for using our app.");
+        }
+        break;
+    }
+  } while (result != "exit");
+}
